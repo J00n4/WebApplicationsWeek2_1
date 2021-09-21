658fc4b9192184df3efa0df32c2a94fbb762c8ac
J00n4 <63159327+J00n4@users.noreply.github.com>
2021-09-21 16:27:32 +0300

    Updated exercises

--- src/index.js
+++ src/index.js
@@ -97,6 +97,27 @@ function removeComments() {
 // Exercise 5 ends
 
 // Week 2 Exercise 4 starts
+/*function Rating() {
+  var newRat = document.createElement("div");
+  var rat = document.createElement("p");
+  var com = document.createElement("p");
+  //newRat.setAttribute("class", "comment");
+  //rat.setAttribute("class", "comment-rating");
+  //com.setAttribute("class", "comment-text");
+  rat.append(
+    document.createTextNode(document.getElementById("rating-menu").value)
+  );
+  console.log(rat);
+  com.append(
+    document.createTextNode(document.getElementById("big-text").value)
+  );
+  newRat.appendChild(rat);
+  newRat.appendChild(com);
+  console.log(newRat);
+  //this.rating = "No rating";
+  //this.comment = "No comment";
+}*/
+
 if (document.readyState !== "loading") {
   console.log("Document is ready");
   addRating();
@@ -114,12 +135,38 @@ function addRating() {
     const ratingsList = document.getElementById("ratings-list");
     console.log("working!");
 
-    var newParagraph3 = document.createElement("div");
+    var newRat = document.createElement("div");
+    var rat = document.createElement("p");
+    var com = document.createElement("p");
+    newRat.className = "comment";
+    rat.className = "comment-rating";
+    com.className = "comment-text";
+    rat.append(
+      document.createTextNode(document.getElementById("rating-menu").value)
+    );
+    console.log(rat);
+    com.append(
+      document.createTextNode(document.getElementById("big-text").value)
+    );
+    newRat.appendChild(rat);
+    newRat.appendChild(com);
+    console.log(newRat);
+    ratingsList.appendChild(newRat);
+    /*var newParagraph3 = new Rating();
+    console.log(newParagraph3.newRat);
+    ratingsList.append(newParagraph3.newRat);*/
+    //newParagraph3.rating = document.getElementById("rating-menu").value;
+    //ewParagraph3.comment = document.getElementById("big-text").value;
+    //var newRating = document.createElement("p");
+    //newRating.append(newParagraph3.rating);
+    //ratingsList.append(newParagraph3.rating);
+    //ratingsList.append(newParagraph3.comment);
+    /*var newParagraph3 = document.createElement("div");
     newParagraph3.innerHTML = document.getElementById("rating-menu").value;
     ratingsList.appendChild(newParagraph3);
     var newParagraph3 = document.createElement("p");
     newParagraph3.innerHTML = document.getElementById("big-text").value;
-    ratingsList.appendChild(newParagraph3);
+    ratingsList.appendChild(newParagraph3);*/
   });
 }
 // Week 2 Exercise 4 ends
